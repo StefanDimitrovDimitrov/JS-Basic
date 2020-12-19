6751198306db2fe9f73fab2a44d35dd03e7edc8a
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-19 11:27:36 +0200

    Initial Commit

---
+++ 07. Project_Creation.js
@@ -0,0 +1,5 @@
+function timeForAProjects(name_ar, projects) {
+    let time_for_projects = projects * 3;
+    console.log(`The architect ${name_ar} will need ${time_for_projects} hours to complete ${projects} project/s.`);
+}
+timeForAProjects("Stefan", "5")
