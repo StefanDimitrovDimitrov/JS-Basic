14dbd725e0082da5918c20e8287644cb02c2fba2
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-19 10:25:58 +0200

    Initial Commit

---
+++ 06. Concatenate_Date.js
@@ -0,0 +1,5 @@
+function concatinateData(fname,lname,age,town) {
+    console.log(`You are ${fname} ${lname}, a ${age}-years old person from ${town}.`);
+}
+
+concatinateData("Stefan", "Dimitrov", "35", "Sofia");
