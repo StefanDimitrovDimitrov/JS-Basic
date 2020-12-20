7de2383ea7407056c477ff2dc081c69c66cf69ce
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-20 15:41:21 +0200

    Initial Commit

---
+++ 02. Conditions/09. Bonus_Score.js
@@ -0,0 +1,23 @@
+function bonus(num) {
+    num = Number(num);
+    let bonus_points = 0.0; 
+
+    if (num > 1000){
+        bonus_point = num * 0.10;
+    } else if(num > 100) {
+        bonuse_point = num * 0.20;
+    } else{
+        bonus_points = 5;
+    }
+    
+    if (num % 2 == 0) {
+        bonus_points = bonus_points + 1;
+    } else if (num % 10 == 5){
+        bonus_points = bonus_points + 2;
+    }
+
+    console.log(bonus_points)
+    console.log(num + bonus_points)
+}
+
+bonus("20")
