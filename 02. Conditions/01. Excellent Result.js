8fb3aefec12020fac2950d97ee81df4f787295b6
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-20 09:24:47 +0200

    move files

---
+++ 02. Conditions/01. Excellent Result.js
@@ -0,0 +1,8 @@
+function score(score_students) {
+    score_studdents = Number(score_students);
+    if (score_students >= 5.50) {
+        console.log("Excellent!");
+    }
+}
+
+score("5.50")
--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-console.log( 2 + 4 + "4" + (2+1))
+console.log( 2 + 4 + "4" + 2+1)
