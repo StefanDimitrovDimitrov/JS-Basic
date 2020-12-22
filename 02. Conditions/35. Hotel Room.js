dee6f8f8c3b4c3316c21057a2366d217fb056a73
Stefan Dimitrov <54206891+StefanDimitrovDimitrov@users.noreply.github.com>
2020-12-22 18:35:51 +0200

    Initial Commit

--- 02. Conditions/35. Hotel Room.js
+++ 02. Conditions/35. Hotel Room.js
@@ -1,2 +1,38 @@
-let a = 119 % 60
-console.log(`${a}`)
+function hotel(month, numNights) {
+    numNights = Number(numNights);
+    let studio = 0
+    let apartment = 0
+
+    if (month == "May" || month == "October") {
+        studio = numNights * 50;
+        apartment = numNights * 65;
+
+        if (numNights > 14) {
+            studio = studio - studio * 0.3;
+            apartment = apartment - apartment * 0.1;
+        } else if (numNights > 7) {
+            studio = studio - studio * 0.05;
+        }
+
+    } else if (month == "June" || month == "September") {
+        studio = numNights * 75.2;
+        apartment = numNights * 68.70;
+        if (numNights > 14) {
+            studio = studio - studio * 0.2;
+            apartment = apartment - apartment * 0.1;
+        }
+    } else if (month == "July" || month == "August") {
+        studio = numNights * 76;
+        apartment = numNights * 77;
+        if (numNights > 14) {
+            apartment = apartment - apartment * 0.1;
+        }
+
+    }
+    console.log(`Apartment: ${apartment.toFixed(2)} lv.`);
+    console.log(`Studio: ${studio.toFixed(2)} lv.`);
+
+}
+
+
+hotel("May", "15")
